c3cded278f47a0527c714266ec3887ca347904d5
Pablo-031 <85771612+Pablo-031@users.noreply.github.com>
2021-06-14 20:24:20 -0500

    mi primer commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const displayValorAnterior = document.getElementById('valor-ant');
+const displayValorActual = document.getElementById('valor-act');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
+const display = new Display(displayValorAnterior, displayValorActual);
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
